72918982be1875ced3c9f89908c23d5769ce551d
cyrilico <jpdamas15@gmail.com>
2018-10-26 09:42:48 +0100

    Add some basic endpoints

--- server/src/controllers/faculty.controller.ts
+++ server/src/controllers/faculty.controller.ts
@@ -24,5 +24,57 @@ export class FacultyController {
     public facultyRepository : FacultyRepository,
   ) {}
 
-  
+  @post('/faculties', {
+    responses: {
+      '200': {
+        description: 'Faculty model instance',
+        content: {'application/json': {'x-ts-type': Faculty}},
+      },
+    },
+  })
+  async create(@requestBody() faculty: Faculty): Promise<Faculty> {
+    return await this.facultyRepository.create(faculty);
+  }
+
+  @get('/faculties', {
+    responses: {
+      '200': {
+        description: 'Array of Faculty model instances',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: {'x-ts-type': Faculty}},
+          },
+        },
+      },
+    },
+  })
+  async find(
+    @param.query.object('filter', getFilterSchemaFor(Faculty)) filter?: Filter,
+  ): Promise<Faculty[]> {
+    return await this.facultyRepository.find(filter);
+  }
+
+  @get('/faculties/{id}/{language}', {
+    responses: {
+      '200': {
+        description: 'Faculty model instance',
+        content: {'application/json': {'x-ts-type': Faculty}},
+      },
+    },
+  })
+  async findById(@param.path.string('id') id: string, @param.path.string('language') language: string): Promise<Faculty> {
+    return await this.facultyRepository.findOne({where: {name: id, language: language}}) || new Faculty();
+  }
+
+  @get('/faculties/{id}/intro/{language}', {
+    responses: {
+      '200': {
+        description: 'Faculty model instance',
+        content: {'application/json': {'x-ts-type': Faculty}},
+      },
+    },
+  })
+  async findIntroById(@param.path.string('id') id: string, @param.path.string('language') language: string): Promise<Faculty> {
+    return await this.facultyRepository.findOne({where: {name: id, language: language}, fields: {short_description: true}}) || new Faculty();
+  }
 }
