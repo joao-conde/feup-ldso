347588990a701f79eca58029bf81fcfcf57b1434
João Seixas <jnuno1997@gmail.com>
2018-11-06 11:24:12 +0000

    Fix linter errors

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 
 import './styles/App.css';
-library.add(faPlusCircle)
+library.add(faPlusCircle);
 
 class App extends Component {
     render() {
