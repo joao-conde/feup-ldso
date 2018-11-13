c3d919a01d373ae52d24148c6e33914710d9c4f4
AndreFCruz <andrecruz97@gmail.com>
2018-11-13 22:39:04 +0000

    added server config scripts

---
+++ configs-server/watcher.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/usr/bin/docker login -u gitlab+deploy-token-26608 -p hHHTA9DoQdWjwhzhd_MZ registry.gitlab.com/ldso18-19/t3g1
+cd /home/andre/LDSO
+ls >> /home/andre/teste.txt
+/usr/local/bin/docker-compose pull 
+/usr/local/bin/docker-compose up -d
