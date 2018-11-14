34550451fc47af30f5a5097679074b7450bdc74d
AndreFCruz <andrecruz97@gmail.com>
2018-11-14 16:31:54 +0000

    fix watcher script

--- configs-server/watcher.sh
+++ configs-server/watcher.sh
@@ -2,5 +2,5 @@
 
 /usr/bin/docker login -u gitlab+deploy-token-26608 -p hHHTA9DoQdWjwhzhd_MZ registry.gitlab.com/ldso18-19/t3g1
 cd /home/andre/LDSO
-/usr/local/bin/docker-compose pull 
-/usr/local/bin/docker-compose up -d
+/usr/bin/docker-compose pull 
+/usr/bin/docker-compose up -d
