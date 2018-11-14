f5ab35bcb0019e26da2c66a9f82b0962d483e0e1
AndreFCruz <andrecruz97@gmail.com>
2018-11-14 16:09:31 +0000

    mobile now uses production server

--- configs-server/watcher.sh
+++ configs-server/watcher.sh
@@ -2,6 +2,5 @@
 
 /usr/bin/docker login -u gitlab+deploy-token-26608 -p hHHTA9DoQdWjwhzhd_MZ registry.gitlab.com/ldso18-19/t3g1
 cd /home/andre/LDSO
-ls >> /home/andre/teste.txt
 /usr/local/bin/docker-compose pull 
 /usr/local/bin/docker-compose up -d
